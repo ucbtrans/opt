b45a44a24dcc94e098980c452d039a4261ce1b16
Gabriel Gomes <gomes@me.berkeley.edu>
2019-07-16 18:44:16 -0700

    fix null ptr exception

--- src/main/java/opt/data/FreewayScenario.java
+++ src/main/java/opt/data/FreewayScenario.java
@@ -94,36 +94,37 @@ public class FreewayScenario {
             segments.get(i).set_name(segment_names==null ? String.format("segment %d",i) : segment_names[i]);
 
         // assign demands
-        for(jaxb.Demand dem : jaxb_scenario.getDemands().getDemand()){
+        if (jaxb_scenario.getDemands()!=null)
+            for(jaxb.Demand dem : jaxb_scenario.getDemands().getDemand()){
 
-            System.out.println("WARNING: DEMANDS NOT IMPLEMENTED");
+                System.out.println("WARNING: DEMANDS NOT IMPLEMENTED");
 
-//            long comm_id = dem.getCommodityId();
-//
-//            if( !get_commodities().containsKey(comm_id))
-//                throw new Exception("Bad commodity id in demand profile");
-//
-//            if(dem.getLinkId()==null)
-//                throw new Exception("Link not specified in demand profile for commodity " + comm_id);
-//
-//            Link or = network.links.get(jd.getLinkId());
-//            if(link==null)
-//                throw new OTMException("Bad link id (" + jd.getLinkId() + ") in demand for commodity " + comm.getId());
-//
-//
-//            this.link = link;
-//            this.path = null;
-//            this.commodity = commodity;
-//
-//            // create a source and add it to the origin
-//            Link origin = get_origin();
-//            source = origin.model.create_source(origin,this,commodity,null);
-//            origin.sources.add(source);
-//
-//            // assume the content to be given in veh/hr
-//            profile = new Profile1D(start_time,dt,values);
-//            profile.multiply(1.0/3600.0);
-        }
+    //            long comm_id = dem.getCommodityId();
+    //
+    //            if( !get_commodities().containsKey(comm_id))
+    //                throw new Exception("Bad commodity id in demand profile");
+    //
+    //            if(dem.getLinkId()==null)
+    //                throw new Exception("Link not specified in demand profile for commodity " + comm_id);
+    //
+    //            Link or = network.links.get(jd.getLinkId());
+    //            if(link==null)
+    //                throw new OTMException("Bad link id (" + jd.getLinkId() + ") in demand for commodity " + comm.getId());
+    //
+    //
+    //            this.link = link;
+    //            this.path = null;
+    //            this.commodity = commodity;
+    //
+    //            // create a source and add it to the origin
+    //            Link origin = get_origin();
+    //            source = origin.model.create_source(origin,this,commodity,null);
+    //            origin.sources.add(source);
+    //
+    //            // assume the content to be given in veh/hr
+    //            profile = new Profile1D(start_time,dt,values);
+    //            profile.multiply(1.0/3600.0);
+            }
 
         // max ids
         reset_max_ids();
