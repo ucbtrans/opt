81c5f54206ff826a48132ac5c07d75233f425423
Alex Kurzhanskiy <akurzhan@gmail.com>
2019-08-16 18:06:10 -0700

    Tooltips

--- src/main/java/opt/config/LinkEditorController.java
+++ src/main/java/opt/config/LinkEditorController.java
@@ -261,6 +261,21 @@ public class LinkEditorController {
     
     @FXML // fx:id="linkEventPane"
     private TitledPane linkEventPane; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="ttAddSectionUpstream"
+    private Tooltip ttAddSectionUpstream; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="ttAddSectionDownstream"
+    private Tooltip ttAddSectionDownstream; // Value injected by FXMLLoader
+    
+     @FXML // fx:id="ttConnectUpstream"
+    private Tooltip ttConnectUpstream; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="ttConnectDownstream"
+    private Tooltip ttConnectDownstream; // Value injected by FXMLLoader
+    
+     @FXML // fx:id="ttDeleteSection"
+    private Tooltip ttDeleteSection; // Value injected by FXMLLoader
 
 
 
@@ -679,9 +694,6 @@ public class LinkEditorController {
            drawRoadSection(); 
         });
         
-        
-        cbBarrier.setTooltip(new Tooltip("Sets barrier between managed and GP lanes"));
-        
     }
     
     
@@ -892,6 +904,31 @@ public class LinkEditorController {
             connectSectionDownstream.setVisible(false);
         else
             connectSectionDownstream.setVisible(true);
+        
+        if (myLink.get_type() == AbstractLink.Type.freeway) {
+             ttDeleteSection.setText("Delete the entire freeway section with all its ramps (if it has any)");
+             ttAddSectionDownstream.setText("Create and attach a new freeway section downstream");
+             if (myLink.get_dn_link() != null) 
+                 ttAddSectionDownstream.setText("Create and insert a new freeway section downstream");
+             ttAddSectionUpstream.setText("Create and attach a new freeway section upstream");
+             if (myLink.get_up_link() != null) 
+                 ttAddSectionDownstream.setText("Create and insert a new freeway section upstream");
+             ttConnectDownstream.setText("Connect to an origin (source) freeway section downstream");
+             ttConnectUpstream.setText("Connect to a destination (sink) freeway section upstream");
+        } else if (myLink.get_type() == AbstractLink.Type.connector) {
+             ttDeleteSection.setText("Delete the connector");
+             ttAddSectionDownstream.setText("Create and attach a new freeway section with an on-ramp downstream");
+             ttAddSectionUpstream.setText("Create and attach a new freeway with an off-ramp section upstream");
+             ttConnectDownstream.setText("Connect to an origin (source) on-ramp downstream");
+             ttConnectUpstream.setText("Connect to a destination (sink) off-ramp section upstream");
+        } else if (myLink.get_type() == AbstractLink.Type.onramp) {
+             ttAddSectionUpstream.setText("Create and attach a new connector upstream");
+             ttConnectUpstream.setText("Connect to a destination (sink) connector upstream");
+        } else { // off-ramp
+             ttAddSectionDownstream.setText("Create and attach a new connector downstream");
+             ttConnectDownstream.setText("Connect to an origin (source) connector downstream");
+        }
+        
         
         
         drawRoadSection();
