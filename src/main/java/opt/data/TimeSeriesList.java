74a1c265868f1cd7e5c07d6c884a43e42a821fae
Gabriel Gomes <gomes@berkeley.edu>
2020-06-09 09:39:37 -0700

    comment out compilation error

--- src/main/java/opt/data/TimeSeriesList.java
+++ src/main/java/opt/data/TimeSeriesList.java
@@ -25,42 +25,43 @@ public class TimeSeriesList {
     }
 
     public TimeSeriesList resample(float newdt){
-        final double epsilon = 1e-3;
-        final float curr_dt = get_dt();
-        final int n = time.size();
-        List<Float> newtime = new ArrayList<>();
-        List<double[]> newvalues = new ArrayList<>();
-
-        for(float currtime=time.get(0);currtime<=time.get(time.size()-1);currtime+=newdt){
-            newtime.add(currtime);
-
-            float float_index = currtime/curr_dt;
-            int index = Math.round(float_index);
-
-
-            if(float_index>=n-1){
-                newvalues.add(values.get(n-1));
-                continue;
-            }
-
-            float lambda = float_index-index;
-            if(Math.abs(lambda)<=epsilon)
-                newvalues.add(values.get(index));
-            else {
-                if(lambda>epsilon){
-                    int ind0 = index;
-                    int ind1 = ind0+1;
-                    newvalues.add( (1-lambda)*values.get(ind0) + lambda*values.get(ind1) );
-                } else {
-                    int ind0 = index-1;
-                    int ind1 = ind0+1;
-                    lambda = -lambda;
-                    newvalues.add( lambda*values.get(ind0) + (1-lambda)*values.get(ind1) );
-                }
-            }
-        }
-
-        return new TimeSeries(newtime,newvalues);
+//        final double epsilon = 1e-3;
+//        final float curr_dt = get_dt();
+//        final int n = time.size();
+//        List<Float> newtime = new ArrayList<>();
+//        List<double[]> newvalues = new ArrayList<>();
+//
+//        for(float currtime=time.get(0);currtime<=time.get(time.size()-1);currtime+=newdt){
+//            newtime.add(currtime);
+//
+//            float float_index = currtime/curr_dt;
+//            int index = Math.round(float_index);
+//
+//
+//            if(float_index>=n-1){
+//                newvalues.add(values.get(n-1));
+//                continue;
+//            }
+//
+//            float lambda = float_index-index;
+//            if(Math.abs(lambda)<=epsilon)
+//                newvalues.add(values.get(index));
+//            else {
+//                if(lambda>epsilon){
+//                    int ind0 = index;
+//                    int ind1 = ind0+1;
+//                    newvalues.add( (1-lambda)*values.get(ind0) + lambda*values.get(ind1) );
+//                } else {
+//                    int ind0 = index-1;
+//                    int ind1 = ind0+1;
+//                    lambda = -lambda;
+//                    newvalues.add( lambda*values.get(ind0) + (1-lambda)*values.get(ind1) );
+//                }
+//            }
+//        }
+//
+//        return new TimeSeries(newtime,newvalues);
+        return null;
     }
 
 
